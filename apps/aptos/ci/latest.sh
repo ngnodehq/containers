3e21bfbc0d38ffd10b4b9f9827dd2e4b9bdbedfe
Anthony PHAM <pham.anthony1@gmail.com>
2024-01-11 20:31:46 +0100

    feat: blockchain aptos (#31)

---
+++ apps/aptos/ci/latest.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+version=$(curl -s https://api.github.com/repos/aptos-labs/aptos-core/releases | jq -r '.[].tag_name' |  grep 'aptos-node-' | sort -V | tail -n 1)
+version="${version#*v}"
+version="${version#*release-}"
+printf "%s" "${version}"
---
+++ apps/aptos/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+#shellcheck disable=SC1091
+test -f "/scripts/umask.sh" && source "/scripts/umask.sh"
+
+# Download genesis.blob and waypoint.txt from Aptos Labs GitHub repository
+APTOS_NETWORK="mainnet"
+curl -s -o /config/genesis.blob https://raw.githubusercontent.com/aptos-labs/aptos-networks/main/${APTOS_NETWORK}/genesis.blob
+curl -s -o /config/waypoint.txt https://raw.githubusercontent.com/aptos-labs/aptos-networks/main/${APTOS_NETWORK}/waypoint.txt
+
+exec \
+    /app/aptos-node --config /config/fullnode.yaml
